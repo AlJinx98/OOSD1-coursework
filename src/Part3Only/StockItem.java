73fbf16727b6f006e494055bce5cb368365efa16
AlJinx98 <56905207+AlJinx98@users.noreply.github.com>
2019-12-08 15:44:26 +0000

    StockItemMNG

--- src/Part3Only/StockItem.java
+++ src/Part3Only/StockItem.java
@@ -1,5 +1,5 @@
 package Part3Only;
-
+// Related to the seperate parts and how their code works, such as the quantity and price 
 public class StockItem {
     // Declare class self variables
     private String fixedStockCode;
@@ -14,23 +14,23 @@ public class StockItem {
     }
 
     public void setQuantity(int quantity) {
-        this.quantity = quantity;
+        this.quantity = quantity; //sets the base quantity to 0? 
     }
 
     public void setPrice(double price) {
-        this.price = price;
+        this.price = price; //Sets the base price to 0?
     }
 
     public String getFixedStockCode() {
-        return fixedStockCode;
+        return fixedStockCode;//?
     }
 
     public int getQuantity() {
-        return quantity;
+        return quantity;//?
     }
 
     public double getPrice() {
-        return price;
+        return price; //?
     }
 
     public double getPriceVAT() {
@@ -38,28 +38,28 @@ public class StockItem {
         double price = getPrice();
 
         double priceAfterVAT = price * (1 + vat);
-        return priceAfterVAT;
+        return priceAfterVAT; //Setting the price after vat, also the price before 
 
     }
 
     public String getStockName() {
         String unknown = "Unknown Stock Name";
-        return unknown;
+        return unknown; // Return value if an unknown stock name is searched for 
     }
 
     public String getStockDescription() {
         String unknown = "Unknown Stock Description";
-        return unknown;
+        return unknown; // Return value if an unknown item is searched for 
     }
 
 
     public void addStock(int addedQuantity) {
 
         if (quantity + addedQuantity < 1) {
-            System.out.println("Stock value cannot be less than one");
+            System.out.println("Stock value cannot be less than one"); // Never less than 1 of an item, 0? 
         }
         else if (quantity + addedQuantity > 100) {
-            System.out.println("Stock value cannot exceed 100");
+            System.out.println("Stock value cannot exceed 100"); // Will never have more than 100 of an item
         }
         else {
             quantity += addedQuantity;
@@ -75,10 +75,10 @@ public class StockItem {
             quantity -= soldQuantity;
             return true;
         }
-    }
+    } // Boolean ensuring you cannot sell an item that there is less than 1 of 
 
     public double getVat() {
-        double standardVat = 0.175;
+        double standardVat = 0.175; //Used to add VAT to price of item 
         return standardVat;
     }
 
@@ -86,7 +86,7 @@ public class StockItem {
         String returnedString = "Stock Code : " + getFixedStockCode() + "\nStock Name : " + getStockName() +
                 "\nStock Description : " + getStockDescription() + "\nQuantity in stock : " + getQuantity() +
                 "\nPrice before VAT : " + getPrice() + "\nPrice after VAT : " + getPriceVAT();
-        return returnedString;
+        return returnedString; //Calculate price using different factors such as VAT, also returns number of stock and the description, Same as others? 
 
     }
 
