e58939555daf7927c5554c233fb70703471158fe
AlJinx98 <56905207+AlJinx98@users.noreply.github.com>
2019-12-08 15:22:46 +0000

    EngineMNG

--- src/Part3Only/Engine.java
+++ src/Part3Only/Engine.java
@@ -1,22 +1,22 @@
 package Part3Only;
 
-public class Engine extends StockItem{
-    private int horsepower;
+public class Engine extends StockItem{ //Class "Engine" 
+    private int horsepower; // Private Integer 
 
-    public Engine(String stockCode, int quantity, double price, int horsepower) {
+    public Engine(String stockCode, int quantity, double price, int horsepower) {//Private variables 
 
         super(stockCode, quantity, price);
-        this.horsepower = horsepower;
+        this.horsepower = horsepower;// super keyword used to invoke immediate parent class constructor.??
     }
 
-    public String getStockName() {return "Engine";}
+    public String getStockName() {return "Engine";} // Returns number of stock for engine 
 
-    public String getStockDescription() {return "car engine";}
+    public String getStockDescription() {return "car engine";} // Returns description of car engine 
 
     public String classToString() {
         String returnedString = "Stock Code : " + getFixedStockCode() + "\nStock Name : " + getStockName() +
                 "\nStock Description : " + getStockDescription() + "\nQuantity in stock : " + getQuantity() +
                 "\nPrice before VAT : " + getPrice() + "\nPrice after VAT : " + getPriceVAT();
-        return returnedString;
+        return returnedString; // Calculates final price of the part and prints it. 
     }
 }
